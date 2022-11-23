f5f56a8b9cd12b844eb5929c04eec865c7fb8794
George Kasparyants <gkasparyants@gmail.com>
2022-11-23 15:54:21 +0300

    add readme && add __main__ host command

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # piedemo
+
+You can simply host your file in Google Drive and use it everywhere you want:
+```commandline
+python -m piedemo host 1.txt
+Host model option
+Transferred:              0 B / 0 B, -, 0 B/s, ETA -
+Checks:                 1 / 1, 100%
+Elapsed time:         1.7s
+Url: 
+https://drive.google.com/open?id=1nN64iU6EY490YMjI3GHfNQhbN4tVmhtu
+Filename: 
+1.txt
+Code:
+PretrainedCheckpoint(gdrive='1nN64iU6EY490YMjI3GHfNQhbN4tVmhtu',
+                     filename='1.txt')
+```
+
---
+++ piedemo/__main__.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+from .checkpoint import rclone_host_file
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='host command',
+                                       description="help with checkpoints saving and loading",
+                                       dest='command')
+    host_parser = subparsers.add_parser('host')
+    host_parser.add_argument('path', type=str)
+    args = parser.parse_args()
+    return args
+
+
+def execute(command, path):
+    if command == 'host':
+        print("Host model option")
+        gdrive = rclone_host_file(path)
+        filename = os.path.basename(path)
+        print("Url: ")
+        print(f"https://drive.google.com/open?id={gdrive}")
+        print("Filename: ")
+        print(filename)
+        print("Code:")
+        print(f"PretrainedCheckpoint(gdrive='{gdrive}',\n"
+              f"                     filename='{filename}')")
+
+
+if __name__ == "__main__":
+    execute(**vars(parse_args()))
