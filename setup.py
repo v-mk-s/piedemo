d309360ad0329db495aeeb9cb9d4c719eadd8459
George Kasparyants <gkasparyants@gmail.com>
2022-12-01 02:13:19 +0300

    V1.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='piedemo',
-    version='1.0.4',
+    version='1.0.5',
     python_requires=f'>=3.6,<3.9',
     description='PyTorch based Web Demo app',
     url='https://github.com/PieDataLabs/piedemo',
